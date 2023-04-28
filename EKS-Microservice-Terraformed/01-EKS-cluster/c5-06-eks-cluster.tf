a5c4c5944835a721bfc25686058aa44e2d1c41a4
Mtekdevops <>
2023-04-28 10:53:18 +0100

    nope!..looks like the exemptions need to be in the resource to work.

--- EKS-Microservice-Terraformed/01-EKS-cluster/c5-06-eks-cluster.tf
+++ EKS-Microservice-Terraformed/01-EKS-cluster/c5-06-eks-cluster.tf
@@ -1,8 +1,8 @@
 
-# Create AWS EKS Cluster
-#checkov:skip=CKV_AWS_58:will enable encryption when K8s secrets are used
-#checkov:skip=CKV_AWS_38:for github actions testing 
+# Create AWS EKS Cluster 
 resource "aws_eks_cluster" "eks_cluster" {
+	# checkov:skip=CKV_AWS_38: for github actions testing
+  # checkov:skip=CKV_AWS_58: will enable encryption when K8s secrets are used 
   name     = "${local.name}-${var.cluster_name}"
   role_arn = aws_iam_role.eks_master_role.arn
   version = var.cluster_version
@@ -10,7 +10,7 @@ resource "aws_eks_cluster" "eks_cluster" {
   vpc_config {
     subnet_ids = module.vpc.public_subnets
     endpoint_private_access = var.cluster_endpoint_private_access
-    #checkov:skip=CKV_AWS_39:public access restricted to a single ip is ok for testing, change for prod 
+    #checkov:skip=CKV_AWS_39:public access for test/ephemeral env only
     endpoint_public_access  = var.cluster_endpoint_public_access
     public_access_cidrs     = ["0.0.0.0/0"]    
   }
